6553a3dd484ea5fbf8108dc229b6b59edafa8397
Arnold Ballesteros <Arnold@arnolds-mac.local>
2015-12-29 20:25:24 -0500

    Initial Commit

---
+++ Basic Tips/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Basic Tips
+//
+//  Created by Arnold Ballesteros on 12/29/15.
+//  Copyright © 2015 Arnold Ballesteros. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
