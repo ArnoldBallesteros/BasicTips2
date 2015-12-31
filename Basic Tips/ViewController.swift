56bc82d867d80da1bea0d0e7788ab35cee1d3b88
Arnold Ballesteros <Arnold@arnolds-mac.local>
2015-12-30 20:58:34 -0500

    First Submission

--- Basic Tips/ViewController.swift
+++ Basic Tips/ViewController.swift
@@ -10,14 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var TipControl: UISegmentedControl!
+    @IBOutlet weak var BillField: UITextField!
+    @IBOutlet weak var TipLabel: UILabel!
+    @IBOutlet weak var TotalLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        TipLabel.text = "$0.00"
+        TotalLabel.text = "$0.00"
+        
     }
 
     override func didReceiveMemoryWarning() {
+        
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        let tipPercentages = [0.18, 0.20, 0.22]
+        let tipPercentage = tipPercentages[TipControl.selectedSegmentIndex]
+        let billAmount = NSString(string: BillField.text!).doubleValue
+        let tip = billAmount * tipPercentage
+        let total = billAmount + tip
+        
+        TipLabel.text = String(format: "$%.2f",  tip)
+        TotalLabel.text = String (format: "$%.2f", total)
+        
+        
+    }
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+
     }
 
 
